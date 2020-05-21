124f9a2079462804bad358ea0c002918d859cc04
shenglu <sxlfhmsl@hotmail.com>
2020-05-21 14:40:04 +0800

    TabPage

--- pages/components/custom-tab-page.js
+++ pages/components/custom-tab-page.js
@@ -1,16 +1,16 @@
 const TabPage = {
 	data() {
 		return {
-			tabInfo: {
+			TabPage_tabInfo: {
 				TabCur: 0,
 				scrollLeft: 0
 			}
 		}
 	},
 	methods: {
-		tabSelect(e) {
-			this.TabCur = e.currentTarget.dataset.id;
-			this.scrollLeft = (e.currentTarget.dataset.id - 1) * 60;
+		TabPage_tabSelect(e) {
+			this.TabPage_TabCur = e.currentTarget.dataset.id;
+			this.TabPage_scrollLeft = (e.currentTarget.dataset.id - 1) * 60;
 		}
 	},
 };
