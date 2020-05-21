d41ae418cef80baf36ce13d86e5bdc742f94d4a6
shenglu <sxlfhmsl@hotmail.com>
2020-05-21 10:37:59 +0800

    add custom-tab-page.vue

---
+++ pages/components/custom-tab-page.vue
@@ -0,0 +1,23 @@
+<template name="custom-tab-page">
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'custom-tab-page',
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- pages/personal/personal.vue
+++ pages/personal/personal.vue
@@ -22,7 +22,7 @@
 
 <script>
 	export default {
-		name: "personal",
+		name: 'personal',
 		data() {
 			return {
 			}
