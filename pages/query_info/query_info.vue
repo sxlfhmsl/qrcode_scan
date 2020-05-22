46b18f836a8f5c1d5cbd600a2d72c945ef9abacd
shenglu <sxlfhmsl@hotmail.com>
2020-05-22 11:57:51 +0800

    qr code can input

--- pages/query_info/query_info.vue
+++ pages/query_info/query_info.vue
@@ -8,6 +8,35 @@
 				<text class="cu-btn lg round text-white cuIcon-qrcode" style="background-color: rgba(255,255,255,0);"></text>
 			</block>
 		</cu-custom>
+		<!-- <view style="text-align: center; margin-top: 30vh;">
+			<view class="basis-sm bg-grey padding-sm radius">
+				<button
+					class="cu-btn bg-olive lg shadow cuIcon-qrcode"
+					style="width: 100vw; height: 100vw; font-size: 80vw;"
+				></button>
+			</view>
+		</view> -->
+		<view>
+			<view class="flex padding justify-center">
+			</view>
+			<view class="flex padding justify-center">
+			</view>
+			<view class="flex padding justify-center">
+			</view>
+			
+			
+			<view class="flex padding justify-center cu-form-group margin-top">
+				<view class="title">编号:</view>
+				<input class="uni-input" placeholder="请输入编号" name="input" :value="itemCode" @confirm="jumpDetails"></input>
+			</view>
+			<view class="flex padding justify-center">
+				<button
+					class="cu-btn bg-olive lg shadow cuIcon-qrcode"
+					style="width: 40vw; height: 40vw; font-size: 22vw;"
+					@tap="qrcodeScan"
+				></button>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -15,11 +44,33 @@
 	export default {
 		data() {
 			return {
-				
+				itemCode: '',
 			}
 		},
 		methods: {
-			
+			/**
+			 * @description 扫描开始
+			 * @param {Object} e
+			 */
+			qrcodeScan: function(e) {
+				uni.scanCode({
+				    onlyFromCamera: true,
+					scanType: ['qrCode'],
+				    success: res => {
+				        console.log('条码类型：' + res.scanType);
+				        console.log('条码内容：' + res.result);
+						this.itemCode = res.result;
+						this.jumpDetails();
+				    }
+				});
+			},
+			/**
+			 * @description 跳转至详情页
+			 */
+			jumpDetails: function() {
+				console.log(this.itemCode);
+				this.itemCode = '';
+			}
 		}
 	}
 </script>
