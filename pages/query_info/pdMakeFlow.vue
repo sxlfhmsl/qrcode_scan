be64e39b835afe7545d028cd0012cfa044cd8dbb
shenglu <sxlfhmsl@hotmail.com>
2020-05-22 16:43:51 +0800

    add make flow item

--- pages/query_info/pdMakeFlow.vue
+++ pages/query_info/pdMakeFlow.vue
@@ -1,15 +1,20 @@
 <template>
-	<view>
-		制作流程
-	</view>
+	<scroll-view class="bg-white padding-sm">
+		<pdMakeFlowItem v-for="(flowItem, index) in flowItems" :key="index" :flowItem="flowItem"></pdMakeFlowItem>
+	</scroll-view>
 </template>
 
 <script>
+	import pdMakeFlowItem from '@/pages/query_info/pdMakeFlowItem';
+	
 	export default {
 		name: 'pdMakeFlow',
+		components: {
+			pdMakeFlowItem
+		},
 		data() {
 			return {
-				
+				flowItems: [{}, {}, {}, {}, {}]
 			}
 		},
 		methods: {
---
+++ pages/query_info/pdMakeFlowItem.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		dsadassssssssssssssss
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'pdMakeFlowItem',
+		data() {
+			return {
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
