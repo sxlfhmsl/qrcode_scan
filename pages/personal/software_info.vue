849c2c4e038286028ade066a8877d30a127b611b
shenglu <sxlfhmsl@hotmail.com>
2020-05-22 10:17:20 +0800

    change software info page

--- pages/personal/software_info.vue
+++ pages/personal/software_info.vue
@@ -5,6 +5,31 @@
 				关于软件
 			</block>
 		</cu-custom>
+		<view class="cu-list menu sm-border">
+			<view class="cu-item arrow">
+				<view class="content">
+					<text class="cuIcon-title text-blue"></text>
+					<text class="text-grey">名称</text>
+					<text class="text-grey custom-text-right">{{this.softwareInfo.defaultTitle}}</text>
+				</view>
+			</view>
+			
+			<view class="cu-item arrow">
+				<view class="content">
+					<text class="cuIcon-people text-blue"></text>
+					<text class="text-grey">版权所属</text>
+					<text class="text-grey custom-text-right">{{this.softwareInfo.owner}}</text>
+				</view>
+			</view>
+			
+			<view class="cu-item arrow">
+				<view class="content">
+					<text class="cuIcon-info text-blue"></text>
+					<text class="text-grey">版本号</text>
+					<text class="text-grey custom-text-right">{{this.softwareInfo.version}}</text>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -22,5 +47,8 @@
 </script>
 
 <style>
-
+	.custom-text-right {
+		position: absolute;
+		right: 60rpx;
+	}
 </style>
