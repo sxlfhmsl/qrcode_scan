ebbdb4d79d41bbe8620452062856144a48e260c9
shenglu <sxlfhmsl@hotmail.com>
2020-05-22 17:35:54 +0800

    commit

--- pages/query_info/pdMakeFlowItem.vue
+++ pages/query_info/pdMakeFlowItem.vue
@@ -1,6 +1,25 @@
 <template>
-	<view>
-		dsadassssssssssssssss
+	<view class="margin">
+		<view class="flex">
+			<view class="flex-treble padding-sm col-auto-line text-white soild" style="width: 100%; background-color: #6495ED;">焊接作业信息</view>
+		</view>
+		
+		<view class="flex">
+			<view class="padding-sm col-auto-line text-white bg-grey solid-left solid-bottom" style="width: 20%">作业时间</view>
+			<view class="padding-sm col-auto-line text-black bg-white solid-left solid-bottom" style="width: 30%"></view>
+			<view class="padding-sm col-auto-line text-white bg-grey solid-left solid-bottom" style="width: 20%">作业人员</view>
+			<view class="padding-sm col-auto-line text-black bg-white solid-left solid-bottom solid-right" style="width: 30%"></view>
+		</view>
+		
+		<view class="flex">
+			<view class="padding-sm col-auto-line text-white bg-grey solid-left solid-bottom" style="width: 20%">作业内容</view>
+			<view class="padding-sm col-auto-line text-black bg-white solid-left solid-bottom solid-right" style="width: 80%"></view>
+		</view>
+		
+		<view class="flex">
+			<view class="padding-sm col-auto-line text-white bg-grey solid-left solid-bottom" style="width: 20%">作业图片</view>
+			<view class="padding-sm col-auto-line text-black bg-white solid-left solid-bottom solid-right" style="width: 80%"></view>
+		</view>
 	</view>
 </template>
 
@@ -11,6 +30,11 @@
 			return {
 			}
 		},
+		props: {
+			flowItem: {
+				required: true,
+			},
+		},
 		methods: {
 			
 		}
@@ -18,5 +42,11 @@
 </script>
 
 <style>
-
+	.col-auto-line {
+		word-wrap:break-word;
+		word-break:normal;
+	}
+	* {
+		font-size: 25rpx !important;
+	}
 </style>
