4d9b6965e18cc7bc86f2ff9f69ba3e9b697418b0
ShengLu <sxlfhmsl@hotmail.com>
2020-05-24 19:37:00 +0800

    commit

--- pages/query_info/pdSendRecv.vue
+++ pages/query_info/pdSendRecv.vue
@@ -1,6 +1,42 @@
 <template>
 	<view>
-		发货/收货
+		<view class="flex margin-left margin-top margin-right">
+			<view class="flex-treble padding-sm col-auto-line text-white soild" style="width: 100%; background-color: #5c9ed6;">发货基本信息</view>
+		</view>
+		
+		<view class="flex margin-left margin-right">
+			<view class="padding-sm col-auto-line text-white solid-left solid-bottom text-center" style="width: 30%; background-color: #dad8d8;">发货时间</view>
+			<view class="padding-sm col-auto-line text-black bg-white solid-left solid-bottom solid-right" style="width: 80%"></view>
+		</view>
+		
+		<view class="flex margin-left margin-right">
+			<view class="padding-sm col-auto-line text-white solid-left solid-bottom text-center" style="width: 30%; background-color: #dad8d8;">发货订单编号</view>
+			<view class="padding-sm col-auto-line text-black bg-white solid-left solid-bottom solid-right" style="width: 80%"></view>
+		</view>
+		
+		<view class="flex margin-left margin-right">
+			<view class="padding-sm col-auto-line text-white solid-left solid-bottom text-center" style="width: 30%; background-color: #dad8d8;">客户单位</view>
+			<view class="padding-sm col-auto-line text-black bg-white solid-left solid-bottom solid-right" style="width: 80%"></view>
+		</view>
+		
+		<view class="flex margin-left margin-right">
+			<view class="padding-sm col-auto-line text-white solid-left solid-bottom text-center" style="width: 30%; background-color: #dad8d8;">发货地址</view>
+			<view class="padding-sm col-auto-line text-black bg-white solid-left solid-bottom solid-right" style="width: 80%"></view>
+		</view>
+		
+		<view class="flex margin-left margin-right">
+			<view class="padding-sm col-auto-line text-white solid-left solid-bottom text-center" style="width: 30%; background-color: #dad8d8;">收货地址</view>
+			<view class="padding-sm col-auto-line text-black bg-white solid-left solid-bottom solid-right" style="width: 80%"></view>
+		</view>
+		
+		<view class="flex margin-left margin-right">
+			<view class="flex-treble padding-sm col-auto-line text-white soild" style="width: 100%; background-color: #5c9ed6;">发货基本信息</view>
+		</view>
+		
+		<view class="flex margin-left margin-right">
+			<view class="padding-sm col-auto-line text-white solid-left solid-bottom text-center" style="width: 30%; background-color: #dad8d8;">收货时间</view>
+			<view class="padding-sm col-auto-line text-black bg-white solid-left solid-bottom solid-right" style="width: 80%"></view>
+		</view>
 	</view>
 </template>
 
@@ -19,5 +55,11 @@
 </script>
 
 <style>
-
+	.col-auto-line {
+		word-wrap:break-word;
+		word-break:normal;
+	}
+	* {
+		font-size: 25rpx !important;
+	}
 </style>
