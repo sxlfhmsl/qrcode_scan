194fcaba3a76bd5d16cec49c0ac968b10bfb4571
ShengLu <sxlfhmsl@hotmail.com>
2020-05-24 18:12:06 +0800

    add in bank info

--- pages/query_info/pdInBankInfo.vue
+++ pages/query_info/pdInBankInfo.vue
@@ -1,6 +1,24 @@
 <template>
 	<view>
-		入库信息
+		<view class="flex">
+			<view class="padding-sm col-auto-line text-white solid-left solid-bottom" style="width: 20%; background-color: #dad8d8;">入库时间</view>
+			<view class="padding-sm col-auto-line text-black bg-white solid-left solid-bottom solid-right" style="width: 80%"></view>
+		</view>
+		
+		<view class="flex">
+			<view class="padding-sm col-auto-line text-white solid-left solid-bottom" style="width: 20%; background-color: #dad8d8;">负责人</view>
+			<view class="padding-sm col-auto-line text-black bg-white solid-left solid-bottom solid-right" style="width: 80%"></view>
+		</view>
+		
+		<view class="flex">
+			<view class="padding-sm col-auto-line text-white solid-left solid-bottom" style="width: 20%; background-color: #dad8d8;">合格证编号</view>
+			<view class="padding-sm col-auto-line text-black bg-white solid-left solid-bottom solid-right" style="width: 80%"></view>
+		</view>
+		
+		<view class="flex">
+			<view class="padding-sm col-auto-line text-white solid-left solid-bottom" style="width: 20%; background-color: #dad8d8;">合格证照片</view>
+			<view class="padding-sm col-auto-line text-black bg-white solid-left solid-bottom solid-right" style="width: 80%"></view>
+		</view>
 	</view>
 </template>
 
@@ -19,5 +37,11 @@
 </script>
 
 <style>
-
+	.col-auto-line {
+		word-wrap:break-word;
+		word-break:normal;
+	}
+	* {
+		font-size: 25rpx !important;
+	}
 </style>
