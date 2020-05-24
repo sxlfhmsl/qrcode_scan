ff94e8f7bd53c1002f8c147aacd57b2dd3c4808a
ShengLu <sxlfhmsl@hotmail.com>
2020-05-24 18:07:41 +0800

    change query info

--- pages/query_info/pdMakeFlowItem.vue
+++ pages/query_info/pdMakeFlowItem.vue
@@ -1,23 +1,23 @@
 <template>
 	<view class="margin">
 		<view class="flex">
-			<view class="flex-treble padding-sm col-auto-line text-white soild" style="width: 100%; background-color: #6495ED;">焊接作业信息</view>
+			<view class="flex-treble padding-sm col-auto-line text-white soild" style="width: 100%; background-color: #5c9ed6;">焊接作业信息</view>
 		</view>
 		
 		<view class="flex">
-			<view class="padding-sm col-auto-line text-white bg-grey solid-left solid-bottom" style="width: 20%">作业时间</view>
+			<view class="padding-sm col-auto-line text-white solid-left solid-bottom" style="width: 20%; background-color: #dad8d8;">作业时间</view>
 			<view class="padding-sm col-auto-line text-black bg-white solid-left solid-bottom" style="width: 30%"></view>
-			<view class="padding-sm col-auto-line text-white bg-grey solid-left solid-bottom" style="width: 20%">作业人员</view>
+			<view class="padding-sm col-auto-line text-white solid-left solid-bottom" style="width: 20%; background-color: #dad8d8;">作业人员</view>
 			<view class="padding-sm col-auto-line text-black bg-white solid-left solid-bottom solid-right" style="width: 30%"></view>
 		</view>
 		
 		<view class="flex">
-			<view class="padding-sm col-auto-line text-white bg-grey solid-left solid-bottom" style="width: 20%">作业内容</view>
+			<view class="padding-sm col-auto-line text-white solid-left solid-bottom" style="width: 20%; background-color: #dad8d8;">作业内容</view>
 			<view class="padding-sm col-auto-line text-black bg-white solid-left solid-bottom solid-right" style="width: 80%"></view>
 		</view>
 		
 		<view class="flex">
-			<view class="padding-sm col-auto-line text-white bg-grey solid-left solid-bottom" style="width: 20%">作业图片</view>
+			<view class="padding-sm col-auto-line text-white solid-left solid-bottom" style="width: 20%; background-color: #dad8d8;">作业图片</view>
 			<view class="padding-sm col-auto-line text-black bg-white solid-left solid-bottom solid-right" style="width: 80%"></view>
 		</view>
 	</view>
