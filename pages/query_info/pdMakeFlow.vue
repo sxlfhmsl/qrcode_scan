fabda8969c364944ed07ccd30b8c24919d30d6e9
ShengLu <sxlfhmsl@hotmail.com>
2020-05-25 23:44:44 +0800

    制作流程修正

--- pages/query_info/pdMakeFlow.vue
+++ pages/query_info/pdMakeFlow.vue
@@ -1,24 +1,49 @@
 <template>
 	<scroll-view class="bg-white padding-sm">
-		<pdMakeFlowItem v-for="(flowItem, index) in flowItems" :key="index" :flowItem="flowItem"></pdMakeFlowItem>
+		<tableNoEditEgA
+			v-for="(tableItem, index) in tableList"
+			:key="index"
+			:rowsBeforeTitle="tableItem.rowsBeforeTitle"
+			:title="tableItem.title" 
+			:rowsAfterTitle="tableItem.rowsAfterTitle" 
+		></tableNoEditEgA>
 	</scroll-view>
 </template>
 
 <script>
-	import pdMakeFlowItem from '@/pages/query_info/pdMakeFlowItem';
+	import tableNoEditEgA from "@/pages/components/tableNoEditEgA";
+	import TableEgAElem from "@/pages/components/tableEgAElem";
 	
 	export default {
 		name: 'pdMakeFlow',
 		components: {
-			pdMakeFlowItem
+			tableNoEditEgA
 		},
 		data() {
 			return {
-				flowItems: [{}, {}, {}, {}, {}]
+				tableList: []
 			}
 		},
 		methods: {
-			
+			/**
+			 * @description 创造数据
+			 */
+			createData: function() {
+				// 例子1
+				this.tableList.push({
+					rowsBeforeTitle: [],
+					title: {color: '#5c9ed6', value: '焊接作业信息'},
+					rowsAfterTitle: [
+						new TableEgAElem('作业时间', '', '25%', '75%', '#dad8d8', 'white'),
+						new TableEgAElem('作业人员', '', '25%', '75%', '#dad8d8', 'white'),
+						new TableEgAElem('作业内容', '', '25%', '75%', '#dad8d8', 'white'),
+						new TableEgAElem('作业图片', '', '25%', '75%', '#dad8d8', 'white')
+					]
+				});
+			}
+		},
+		mounted:function(){
+			this.createData();
 		}
 	}
 </script>
