9711b5faa0643b2a846d20c4f8ac45e84f1f0f55
shenglu <sxlfhmsl@hotmail.com>
2020-05-26 09:49:15 +0800

    add explain

--- pages/components/tableElem.js
+++ pages/components/tableElem.js
@@ -1,4 +1,10 @@
 class TableElem {
+	/**
+	 * @description 表格元素
+	 * @param {Object} classContent 类
+	 * @param {Object} styleContent 样式
+	 * @param {Object} content 内容
+	 */
 	constructor(classContent, styleContent, content) {
 	    this.classContent = classContent;
 		this.styleContent = styleContent;
