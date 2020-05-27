b358b3d0c8b582eb72cbe166c86c8fc778bdff49
shenglu <sxlfhmsl@hotmail.com>
2020-05-27 15:28:37 +0800

    fix bug

--- pages/components/network/request/url.js
+++ pages/components/network/request/url.js
@@ -2,9 +2,10 @@
  * @description 请求地址,此下有统一之拦截
  */
 class Url {
-	constructor(url, type) {
+	constructor(url, type, pathParams) {
 	    this._url = url;
 		this._type = type;
+		this._pathParams = pathParams;
 		this._urlParams = {};
 	};
 	
@@ -19,7 +20,13 @@ class Url {
 				paramList.push(param + '=' + this._urlParams[param]);
 			}
 		});
-		return Url.baseUrl + this._url + (paramList.length > 0? ('?' + paramList.join('&')): '');
+		return Url.baseUrl + this._url 
+			+ (
+				this._pathParams !== undefined && this._pathParams !== null && this._pathParams.length > 0? 
+				('/' + this._pathParams.join('/')): 
+				''
+			)
+			+ (paramList.length > 0? ('?' + paramList.join('&')): '');
 	};
 	
 	get type() {
