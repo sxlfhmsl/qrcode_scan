1ae5daebb3ccf295beecac4f0c44d5ee3989a28f
shenglu <sxlfhmsl@hotmail.com>
2020-05-27 11:02:37 +0800

    add material.js

---
+++ pages/components/network/request/material.js
@@ -0,0 +1,12 @@
+import BaseRequest from "@/pages/components/network/request/base";
+import RequestType from "@/pages/components/network/request/type";
+import Url from "@/pages/components/network/request/url";
+
+/**
+ * @description 原材料请求类
+ */
+class MaterialRequest extends BaseRequest {
+	
+};
+
+export default MaterialRequest;
