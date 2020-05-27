5fc417fedc8df297eeccfc80573cad44d84c1d36
shenglu <sxlfhmsl@hotmail.com>
2020-05-27 15:21:53 +0800

    recv send

--- pages/recv/recv.vue
+++ pages/recv/recv.vue
@@ -2,10 +2,10 @@
 	<view>
 		<cu-custom bgColor="bg-gradual-blue" :isBack="true">
 			<block slot="content">
-				收货/发货
+				收货
 			</block>
 			<block slot="right">
-				<text class="cu-btn lg round text-white cuIcon-deliver" style="background-color: rgba(255,255,255,0);"></text>
+				<text class="cu-btn lg round text-white cuIcon-goods" style="background-color: rgba(255,255,255,0);"></text>
 			</block>
 		</cu-custom>
 	</view>
---
+++ pages/send/send.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<cu-custom bgColor="bg-gradual-blue" :isBack="true">
+			<block slot="content">
+				发货
+			</block>
+			<block slot="right">
+				<text class="cu-btn lg round text-white cuIcon-deliver" style="background-color: rgba(255,255,255,0);"></text>
+			</block>
+		</cu-custom>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
