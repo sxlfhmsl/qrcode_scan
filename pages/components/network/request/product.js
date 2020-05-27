fd6b4e0ce7409d104076079459723afc39061291
shenglu <sxlfhmsl@hotmail.com>
2020-05-27 14:07:03 +0800

    madeList

--- pages/components/network/request/product.js
+++ pages/components/network/request/product.js
@@ -13,6 +13,8 @@ class ProductRequest extends BaseRequest {
 		this.productByCodeUrl = new RequestType('product/productByCode', RequestType.NORMAL | RequestType.NEED_TOKEN);
 		this.selectMaterialUrl = new RequestType('product/selectMaterial', RequestType.NORMAL | RequestType.NEED_TOKEN);
 		this.selectWorkerUrl = new RequestType('product/selectWorker', RequestType.NORMAL | RequestType.NEED_TOKEN);
+		this.madeUrl = new RequestType('product/made', RequestType.NORMAL | RequestType.NEED_TOKEN);
+		this.madeListUrl = new RequestType('product/madeList', RequestType.NORMAL | RequestType.NEED_TOKEN);
 	};
 	
 	/**
@@ -80,6 +82,50 @@ class ProductRequest extends BaseRequest {
 	selectWorker(successCallback) {
 		this.basePost(
 			this.selectWorkerUrl,
+			{},
+			(result) => {
+				this.baseResultProcess(result, successCallback);
+			}
+		);
+	};
+	
+	/**
+	 * @description 制作编辑保存
+	 * @param {Object} id 制作id（productMade的对象id）
+	 * @param {Object} productId 产品id 
+	 * @param {Object} madeParams 制作参数
+	 * @param {Object} successCallback 成功回调
+	 */
+	made(id, productId, madeParams, successCallback) {
+		madeParams['id'] = id;
+		madeParams['productId'] = productId;
+		this.madeUrl.urlParams = madeParams;
+		this.basePost(
+			this.madeUrl,
+			{},
+			(result) => {
+				this.baseResultProcess(result, successCallback);
+			}
+		);
+	};
+	
+	/**
+	 * @description 制作流程列表（包含发货列表）
+	 * @param {Object} code 产品编号
+	 * @param {Object} status 状态 状态:0-待制作；1-制作中；2-已入库；3-已发货；4-已报废
+	 * @param {Object} page 当前页数
+	 * @param {Object} limit 每页大小
+	 * @param {Object} successCallback
+	 */
+	madeList(code, status, page, limit, successCallback) {
+		this.madeListUrl.urlParams = {
+			'code': code,
+			'status': status,
+			'page': page,
+			'limit': limit
+		};
+		this.basePost(
+			this.madeListUrl,
 			{},
 			(result) => {
 				this.baseResultProcess(result, successCallback);
