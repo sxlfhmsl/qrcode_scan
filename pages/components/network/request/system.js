9686a81bcdd528166e9f0fa93850b0fbe0442a7f
shenglu <sxlfhmsl@hotmail.com>
2020-05-27 10:35:02 +0800

    add app version get

---
+++ pages/components/network/request/system.js
@@ -0,0 +1,37 @@
+import BaseRequest from "@/pages/components/network/request/base";
+import RequestType from "@/pages/components/network/request/type";
+import Url from "@/pages/components/network/request/url";
+
+/**
+ * @description 系统请求类，用于获取app版本号等
+ */
+class SystemRequest extends BaseRequest {
+	
+	constructor() {
+	    super();
+		this.appCurrentVersionUrl = new Url('appVersion/current', RequestType.NORMAL | RequestType.NEED_TOKEN);
+	};
+	
+	/**
+	 * @description 获取app版本信息
+	 * @param {Function} successCallback 成功执行回调
+	 */
+	appCurrentVersion(successCallback) {
+		this.basePost(
+			this.appCurrentVersionUrl,
+			{},
+			(result) => {
+				if (result.data.code == 200) {
+					successCallback(result.data.data);
+				}
+				else {
+					uni.showToast({
+						title: result.data.message
+					});
+				}
+			}
+		);
+	};
+};
+
+export default SystemRequest;
