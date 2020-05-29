d12aef910f0654abab043e50e2d6311e6ff3aa71
shenglu <sxlfhmsl@hotmail.com>
2020-05-29 17:01:50 +0800

    add resetSearchFilter material

--- pages/make/makeMain.vue
+++ pages/make/makeMain.vue
@@ -15,7 +15,7 @@
 				width: 100%;
 				z-index: 1024;
 			" :style="'top:' + CustomBar + 'px'">
-				<pdBaseInfo></pdBaseInfo>
+				<pdBaseInfo :itemData="pdBaseInfoData"></pdBaseInfo>
 				<scroll-view scroll-x class="bg-white nav solid-top solid-bottom text-center" scroll-with-animation :scroll-left="TabPage_tabInfo.scrollLeft">
 					<view 
 						class="cu-item"
@@ -33,7 +33,14 @@
 		
 		
 		<scroll-view v-show="TabPage_tabInfo.TabCur==='pdSource'">
+			<view class="text-center flex">
+				<input class="radius margin" placeholder="材料编号" style="max-width: 30%; border-bottom: grey solid 1px;" v-model="code"/>
+				<input class="radius margin" placeholder="品种名称" style="max-width: 30%; border-bottom: grey solid 1px;" v-model="materialType"/>
+				<input class="radius margin" placeholder="进场时间" style="max-width: 30%; border-bottom: grey solid 1px;" v-model="enterDate" disabled="true"/>
+			</view>
+			
 			<view class="text-center">
+				<button class="cu-btn bg-blue round lg shadow margin" @tap="resetSearchFilter">重置</button>
 				<button class="cu-btn bg-blue round lg shadow margin" @tap="toNextStep">下一步</button>
 			</view>
 		</scroll-view>
@@ -50,6 +57,9 @@
 <script>
 	import pdBaseInfo from '@/pages/components/pdBaseInfo';
 	import TabPage from "@/pages/components/custom-tab-page";
+	import ProductRequest from "@/pages/components/network/request/product";
+	import MaterialRequest from "@/pages/components/network/request/material";
+	
 	export default {
 		components: {
 			pdBaseInfo
@@ -58,7 +68,18 @@
 		extends: TabPage,
 		data() {
 			return {
-				'title': ''
+				'title': '',                                                // 标题
+				'productRequest': new ProductRequest(),                     // 产品网络请求对象
+				'materialRequest': new MaterialRequest(),                   // 原材料网络请求对象
+				'productId': null,                                          // 产品id
+				'rawProductData': null,                                     // 产品数据
+				'pdBaseInfoData': {},                                       // 基础信息绑定数据对象
+				'choosedSouceIds': [],                                      // 选中的原材料id
+				'choosedSouceList': [],                                     // 选中的原材料列表
+				'souceList': [],                                            // 原材料列表
+				'code': '',                                                 // 编码
+				'materialType': '',                                         // 品种名称
+				'enterDate': '',                                            // 进场日期
 			}
 		},
 		methods: {
@@ -82,8 +103,59 @@
 			 * @param {Object} e 事件对象
 			 */
 			saveMake: function(e) {
-				
+				if (this.productId !== null) {
+					
+				}
+			},
+			resetSearchFilter: function(e) {
+				this.code = '';
+				this.materialType = '';
+				this.enterDate = '';
+				this.loadSourceData();
+			},
+			/**
+			 * @description 加载产品信息
+			 */
+			loadProductInfo: function() {
+				this.productRequest.detailById(this.projectId, data => {
+					this.rawProductData = data;
+					if (data !== null) {
+						this.title = data.product.name;
+						this.pdBaseInfoData = data.product;
+					}
+				});
+			},
+			/**
+			 * @description 加载原材料信息
+			 */
+			loadSourceData: function() {
+				this.productRequest.selectMaterial(
+					this.code, this.materialType, this.enterDate, 1, 99999, data => {
+						if (data != null) {
+							this.souceList = data.records;
+						}
+				});
+			},
+			/**
+			 * @description 加载已选择原材料
+			 */
+			loadChoosedSource: function() {
+				this.materialRequest.list(this.projectId, data => {
+					if (data != null) {
+						this.choosedSouceList = data;
+						this.choosedSouceCodes = [];
+						this.choosedSouceList.forEach(item => {
+							this.choosedSouceIds.push(item.materialId);
+						});
+					}
+				});
 			}
+		},
+		onLoad: function(option) {
+			this.projectId = (option.projectId? option.projectId: this.projectId);
+			this.loadProductInfo();
+			this.loadSourceData();
+			this.loadChoosedSource();
 		},
 		mounted:function(){
 			this.TabPage_tabInfo.TabItems = [{
