f51089b450a5c0cae3053d2a17aae5a98bca1323
shenglu <sxlfhmsl@hotmail.com>
2020-06-08 10:29:50 +0800

    fix bug

--- pages/components/custom-tab-page.js
+++ pages/components/custom-tab-page.js
@@ -12,6 +12,11 @@ const TabPage = {
 		TabPage_tabSelect(e) {
 			this.TabPage_tabInfo.TabCur = e.currentTarget.dataset.id;
 			this.TabPage_tabInfo.scrollLeft = (e.currentTarget.dataset.id - 1) * 60;
+			
+			// 页面切换强制数据刷新
+			if (this.flushData) {
+				this.flushData();
+			}
 		}
 	},
 };
