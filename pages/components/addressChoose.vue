ddb51c18a5430ec680e58dd63609f2ee84571504
shenglu <sxlfhmsl@hotmail.com>
2020-06-08 17:33:38 +0800

    map choose

--- pages/components/addressChoose.vue
+++ pages/components/addressChoose.vue
@@ -12,39 +12,59 @@
 		<view class="page-body">
 			<view class="page-section page-section-gap">
 				<map 
+					ref="locationMap"
+					id="locationMap"
 					style="width: 100%;" 
-					:style="'height: calc(100vh - ' + CustomBar + 'rpx);'" 
+					:style="'height: calc(100vh - ' + (CustomBar * 2) + 'rpx);'" 
 					:latitude="center.latitude" 
 					:longitude="center.longitude" 
 					:markers="covers">
+					<cover-view
+						class="font-content-simsun"
+						style="
+							background-color: rgba(255, 255, 255, 0.8);
+							border-radius: 20rpx;
+							color: #000000;
+							font-size: 30rpx;
+							border: #000000 solid 2rpx;
+							position: fixed;
+							z-index: 100;
+							left: 20rpx;
+							height: 80rpx;
+							width: 500rpx;
+							top: 100rpx;
+							font-size: 30rpx;
+							line-height: 2.5em;
+						"
+						:style="'top: ' + (CustomBar * 2) + 'rpx;width: calc(100vw - ' + 140 + 'rpx);'"
+					>
+						{{'  ' + covers[0].title}}
+					</cover-view>
+					
+					<cover-image
+						src="~@/static/location.png"
+						style="border-radius: 40rpx; border: gray solid 1rpx; position: fixed; z-index: 100;right: 20rpx; width: 80rpx; height: 80rpx;"
+						:style="'top: ' + (CustomBar * 2) + 'rpx;' "
+						@tap="toLocation"
+					>
+					</cover-image>
+					
+					<cover-image
+						src="~@/static/bigger.png"
+						style="border: #000000 solid 2rpx; position: fixed; z-index: 100;right: 20rpx; width: 80rpx; height: 80rpx; bottom: 120rpx;"
+						@tap="lower"
+					>
+					</cover-image>
+					
+					<cover-image
+						src="~@/static/lower.png"
+						style="border: #000000 solid 2rpx; position: fixed; z-index: 100;right: 20rpx; width: 80rpx; height: 80rpx; bottom: 40rpx;"
+						@tap="toLocation"
+					>
+					</cover-image>
 				</map>
 			</view>
 		</view>
-		
-		<input
-			class="font-content-simsun"
-			style="
-				background-color: rgba(255, 255, 255, 0.8);
-				border-radius: 20rpx;
-				color: #000000;
-				font-size: 30rpx;
-				border: #000000 solid 2rpx;
-				position: fixed;
-				z-index: 100;
-				left: 20rpx;
-				height: 80rpx;
-				width: 500rpx;
-				top: 100rpx;"
-			:style="'top: ' + (CustomBar + 70) + 'rpx;width: calc(100vw - ' + 140 + 'rpx);'"
-			type="text"
-			value="" />
-		
-		<button
-			class="cuIcon cuIcon-locationfill cu-btn lg line-blue"
-			style="position: fixed; z-index: 100;right: 20rpx;"
-			:style="'top: ' + (CustomBar + 70) + 'rpx;' "
-			@tap="toLocation"
-		></button>
 	</view>
 </template>
 
@@ -61,17 +81,32 @@
 					'longitude': 104.06575163597108,
 					'latitude': 30.65744964620209,
 					'title': '天府广场',
-				}]
+					'iconPath': '../../static/pos.png',
+				}],
+				map: null
 			}
 		},
 		methods: {
 			toLocation: function() {
 				
+			},
+			
+			bigger: function() {
+				
+			},
+			
+			lower: function() {
+				
 			}
+		},
+		mounted: function(){
+		},
+		onReady: function() {
+			console.log(this.$refs.locationMap);
 		}
 	}
 </script>
 
 <style>
-
+	
 </style>
