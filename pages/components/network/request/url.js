fb60be321bd17f84f9e1019f834b8e87cfdcda6e
shenglu <sxlfhmsl@hotmail.com>
2020-07-01 17:20:01 +0800

    switch url

--- pages/components/network/request/url.js
+++ pages/components/network/request/url.js
@@ -10,11 +10,11 @@ class Url {
 	};
 	
 	static get baseUrl() {
-		return 'http://47.108.78.92:8180/cmpApi/';
+		return 'http://47.108.70.55:8180/cmpApi/';
 	};
 	
 	static get resBaseUrl() {
-		return 'http://47.108.78.92:8180';
+		return 'http://47.108.70.55:8180';
 	};
 	
 	get url() {
